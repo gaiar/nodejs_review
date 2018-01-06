5b678ae855485aaf28f359b6fd5d4d5c0ac9b8f7
Ekaterina Anisimova <ekate.anisimova@gmail.com>
2018-01-06 11:59:17 +0100

    Complete tasks 1 and 2.

--- routes/pins.js
+++ routes/pins.js
@@ -15,7 +15,7 @@ const codes = require('../restapi/http-codes');
 const HttpError = require('../restapi/http-error.js');
 
 // TODO add here your require for your own model file
-const PinsModel = require('../models/pin');
+const PinModel = require('../models/pin');
 
 const pins = express.Router();
 
@@ -26,21 +26,31 @@ pins.route('/')
     .get((req, res, next) => {
         // TODO replace store and use mongoose/MongoDB
         // res.locals.items = store.select(storeKey);
-        res.locals.processed = true;
-        logger("GET fetched items");
-        next();
+        PinModel.find({}, (err, items) => {
+            res.status(codes.success).json(items);
+            res.locals.processed = true;
+            logger("GET fetched items");
+
+        });
+
     })
     .post((req,res,next) => {
         req.body.timestamp = new Date().getTime();
         // TODO replace store and use mongoose/MongoDB
         // var result = store.insert(storeKey, req.body);
-
-        res.status(codes.created);
+        let pin = new PinModel(req.body);
+        pin.save(err => {
+            if (err) {
+                return next(err);
+            }
+            res.status(codes.created).json(pin);
+            res.locals.processed = true;
+            //next();
+        });
 
         // TODO replace store and use mongoose/MongoDB
         // res.locals.items = store.select(storeKey, id);
-        res.locals.processed = true;
-        next();
+
     })
     .all((req, res, next) => {
         if (res.locals.processed) {
@@ -56,28 +66,53 @@ pins.route('/:id')
     .get((req, res,next) => {
         // TODO replace store and use mongoose/MongoDB
         // res.locals.items = store.select(storeKey, req.params.id);
-        res.locals.processed = true;
-        next();
+        PinModel.findById(req.params.id, (err, item) => {
+            if (err) {
+                let err = new HttpError('No element with id ' + req.params.id, codes.notfound);
+                return next(err);
+            }
+            res.status(codes.success).json(item);
+            res.locals.processed = true;
+        });
+
+        //next();
     })
     .put((req, res,next) => {
 
         // TODO replace store and use mongoose/MongoDB
         // store.replace(storeKey, req.body.id, req.body);
-        res.status(codes.success);
+        PinModel.findByIdAndUpdate(req.params.id, req.body, {runValidators: true, new: true}, (err, item) => {
+            if (err) {
+                let err = new HttpError('No element to update with id ' + req.params.id, codes.notfound);
+                return next(err);
+            }
+            res.status(codes.success).json(item);
+            res.locals.processed = true;
+        });
+        // res.status(codes.success);
         // res.locals.items = store.select(storeKey, id);
-        res.locals.processed = true;
-        next();
+
+        //next();
     })
     .delete((req,res,next) => {
         // TODO replace store and use mongoose/MongoDB
         // store.remove(storeKey, id);
 
         // ...
-        //    var err = new HttpError('No element to delete with id ' + req.params.id, codes.notfound);
+        //let err = new HttpError('No element to delete with id ' + req.params.id, codes.notfound);
         //    next(err);
         // ...
-        res.locals.processed = true;
-        next();
+        PinModel.findByIdAndRemove(req.params.id, (err, item) => {
+
+            if (err) {
+                let err = new HttpError('No element to delete with id ' + req.params.id, codes.notfound);
+                return next(err);
+            }
+            res.status(codes.success).json(item);
+            res.locals.processed = true;
+        });
+
+        //next();
     })
 
     .all((req, res, next) => {
